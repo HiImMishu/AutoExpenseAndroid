d3517506e0f40e3df2288cb5c758c12f39981fbe
Michał <michal.misiak@op.pl>
2020-11-19 13:52:29 +0100

    Added NavigateUp button on Toolbar

--- app/src/main/java/com/misiak/android/autoexpense/MainActivity.kt
+++ app/src/main/java/com/misiak/android/autoexpense/MainActivity.kt
@@ -2,7 +2,10 @@ package com.misiak.android.autoexpense
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.misiak.android.autoexpense.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        findNavController(R.id.nav_host_fragment).addOnDestinationChangedListener{controller, destination, arguments ->
+            if (destination.id == R.id.mainScreenFragment)
+                supportActionBar?.setDisplayHomeAsUpEnabled(false)
+            else
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home)
+            return onNavigateUp()
+
+        return super.onOptionsItemSelected(item)
     }
 }
